afdbd2669b39d6a3da3d10bfae873e0fc8f73ff3
madmanforces <hwangcc1019@naver.com>
2022-02-25 19:53:06 +0900

    restart again!!

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,23 +1,14 @@
 import { atom,selector } from "recoil";
 
-export interface IToDo {
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE";
-}
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    return [
-      toDos.filter((toDo) => toDo.category === "TO_DO"),
-      toDos.filter((toDo) => toDo.category === "DOING"),
-      toDos.filter((toDo) => toDo.category === "DONE"),
-    ];
+    const minutes = get(minuteState);
+    return minutes / 60;
   },
 });
